a821606637aefca863cceb9b6afaf4134ce65974
radin <radinErfanfar@gmail.com>
2022-12-17 11:47:08 +0330

    Ui of HomePage a little compelted.

--- lib/ui/homePage.dart
+++ lib/ui/homePage.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:task_02/ui/ui_helper/ThemeSwitcher.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
-    );
+    var primaryColor = Theme.of(context).primaryColor;
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+        drawer: const Drawer(),
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+          actions: const [
+            ThemeSwitcher(),
+          ],
+          title: const Text('just a Title as String'),
+          centerTitle: true,
+          titleTextStyle: textTheme.titleLarge,
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [],
+            ),
+          ),
+        ));
   }
 }
