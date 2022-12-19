3286e4afa8b20ec307c68139a01aef983c63aa03
radin <radinErfanfar@gmail.com>
2022-12-19 18:33:47 +0330

    working on api

--- lib/network/apiProvider.dart
+++ lib/network/apiProvider.dart
@@ -1,2 +1,11 @@
-  //954
-  //https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=5000&sortBy=market_cap&sortType=desc&convert=USD,BTC,ETH&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap
+//https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=5000&sortBy=market_cap&sortType=desc&convert=USD,BTC,ETH&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap
+import 'package:dio/dio.dart';
+
+class ApiProivder {
+  dynamic getTopMarketCapData() async {
+    var response;
+    response = await Dio().get(
+        'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=15&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap');
+    return response;
+  }
+}
---
+++ lib/providers/cryptoDataProvider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:task_02/network/apiProvider.dart';
+
+class CryptoDataProvider extends ChangeNotifier {
+  ApiProivder apiProivder = ApiProivder();
+  getTopMarketCapData() {}
+}
