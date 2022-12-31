83b94482ab0216c4883ba6b9197839a55cd0d13b
radin <radinErfanfar@gmail.com>
2022-12-31 17:09:21 +0330

    working on marketP shimmer 31dec

--- lib/ui/ui_helper/ShimmerMartketWidget.dart
+++ lib/ui/ui_helper/ShimmerMartketWidget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -7,122 +6,32 @@ class ShimmerMarketWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: Shimmer.fromColors(
-          baseColor: Colors.grey,
-          highlightColor: Colors.white,
-          child: Column(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Shimmer.fromColors(
+          baseColor: Colors.black,
+          highlightColor: Colors.green,
+          child: ListView(
+            padding: const EdgeInsets.all(15),
             children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0,left: 10,right: 10),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 55,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                    ),                                               ),
-                ),
+              Container(
+                height: 50,
+                child: const Center(child: Text('Entry A')),
               ),
-              Expanded(
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (context, index){
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(top: 8.0,bottom: 8,left: 8),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.white,
-                              radius: 30,
-                              child: Icon(Icons.add),
-                            ),
-                          ),
-                          Flexible(
-                            fit: FlexFit.tight,
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 8.0, left: 8),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: 50,
-                                    height: 15,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: Colors.white,
-                                      ),                                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: SizedBox(
-                                      width: 25,
-                                      height: 15,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(10),
-                                          color: Colors.white,
-                                        ),                                                       ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Flexible(
-                            fit: FlexFit.tight,
-                            child: SizedBox(
-                              width: 70,
-                              height: 40,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: Colors.white,
-                                ),                                               ),
-                            ),
-                          ),
-                          Flexible(
-                            fit: FlexFit.tight,
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  SizedBox(
-                                    width: 50,
-                                    height: 15,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: Colors.white,
-                                      ),                                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: SizedBox(
-                                      width: 25,
-                                      height: 15,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(10),
-                                          color: Colors.white,
-                                        ),                                                       ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      );
-                    }),
+              Container(
+                height: 50,
+                child: const Center(child: Text('Entry B')),
+              ),
+              Container(
+                height: 50,
+                child: const Center(child: Text('Entry C')),
               ),
             ],
-          )
+          ),
+        ),
       ),
     );
   }
